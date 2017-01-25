089a074dcc0f0de14e74c24c1fc29eb9a2b96973
mlindhe <m.j.lindhe@gmail.com>
2017-01-25 12:34:17 +0100

    add console.log

--- lab1/js/sp.js
+++ lab1/js/sp.js
@@ -48,7 +48,7 @@ function sp(){
     x.domain([0, d3.max(data, function(d){ return d[0] })]);
     y.domain([0, d3.max(data, function(d){ return d[1] })]);
     
-    console.log(data);
+    console.log(self.data);
     draw();
 
     });
